0712a74a84c17b0ef2e883e94dc0004c02d87568
TimourIc <timour-i@hotmail.com>
2024-01-14 21:59:55 +0100

    added plotting functionality

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 Simple repository to play around with different computer vision models and compare their performance on common benchmarks. 
+
+1) Create venv and install requirements:
+
+```python
+    python3 -m venv venv
+    pip install -r requirements.txt 
+```
+
+2) Hyperparam turning and training best models datasets:
+```python
+    make tune_and_train_all_MNIST
+    make tune_and_train_all_CIFAR100
+```
+3) Visualize results:
+```python
+    make make_figures
+```
+
+
+
--- experiments/plotting/main.py
+++ experiments/plotting/main.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt 
 import yaml
-import pickle
-from itertools import product
+from experiments.plotting.plot_functions import FigureMaker
 
 # IMPORT REPO PATHS
 with open("config/config.yaml", "r") as file:
@@ -11,38 +10,13 @@ models_path = config["PATHS"]["models_path"]
 figure_path=config["PATHS"]["figures_path"]
 dataset_names=["MNIST", "CIFAR100"]
 model_names = ["FNN", "AlexNet"]
-training_loss_paths= [ f"{models_path}/{dataset_name}/{model_name}_model/{dataset_name}_training_loss.pickle" for (model_name, dataset_name) in product(model_names, dataset_names)]
 
-
-def load_results(dataset_names, model_names):
-
-    train_dict={ (model_name, dataset_name):{} for (model_name, dataset_name) in product(model_names, dataset_names) } 
-    for dataset_name in dataset_names:
-        for model_name in model_names:
-            pickle_path=f"{models_path}/{dataset_name}/{model_name}_model/{dataset_name}_training_loss.pickle"
-            with open(pickle_path, 'rb') as f:
-                train_dict[(model_name, dataset_name)]= pickle.load(f)
-    return train_dict
-
-def plot_training_curves(train_loss, val_loss, test_loss):
-
-    epochs=range(0,len(train_loss))
-    plt.plot(epochs, train_loss, label="train loss")
-    plt.plot(epochs, val_loss, label= "validation loss")
-    plt.legend()
-    plt.plot()
-    plt.show()
-    
 
 if __name__=="__main__":
+    figmaker=FigureMaker(dataset_names=dataset_names, model_names=model_names)
+    figmaker.load_results(models_path=models_path)
+    figmaker.plot_training_curves(save_path=figure_path, figure_name="training_features")
+    figmaker.plot_test_results(save_path=figure_path, figure_name="test_comparison")
 
-    train_dict= load_results(dataset_names, model_names)
-
-    model_name=model_names[0]
-    for dataset_name in dataset_names:
-        train_loss=train_dict[(model_name,dataset_name)]["training_loss"]
-        val_loss=train_dict[(model_name,dataset_name)]["validation_loss"]
-        test_loss=train_dict[(model_name,dataset_name)]["test_loss"]
-        plot_training_curves(train_loss, val_loss, test_loss)
 
  
